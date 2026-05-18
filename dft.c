2f79c67aa2bab4f99164bd53cf12e23187b74b26
kenddoku <kenddoku@gmail.com>
2026-05-19 01:02:11 +0200

    fixed bug in signal and samples generation

--- dft.c
+++ dft.c
@@ -71,40 +71,118 @@ int save_dft_results(double sampling_freq, double* freq_magnitudes, double* phas
     return 0;
 };
 
-int main() {
+// Since I dont use any analog devices I simulate analog signal by generating
+// much more samples of desired curve (combination of sine waves)
+int generate_signal(
+    double* signal, size_t signal_len, double sampling_time,
+    double sine_freqs[], double initial_phases[], size_t num_freqs
+) {
+    for(size_t freq_index=0; freq_index<num_freqs; freq_index++)
+        for(size_t signal_point=0; signal_point<signal_len; signal_point++) {
+            double t = signal_point * sampling_time / signal_len;
+            signal[signal_point] += sin(2*M_PI * sine_freqs[freq_index] * t + initial_phases[freq_index]);
+    }
 
-    double SAMPLING_FREQ = 100.0;   // [Hz]
-    double SAMPLING_TIME = 10;    // [s]
-    size_t SAMPLES_LEN = SAMPLING_FREQ * SAMPLING_TIME;
-    
-    // Allocating memory for samples vector and zero initializing it ==============================
-    double *samples = (double*)calloc(SAMPLES_LEN, sizeof(double));
-
-    // Generating a simple sine wave (already sampled for simplicy; at least for now) =============
-    // and saving it to a file
-    double sine_freq = 10.0;
-    double initial_phase = 0.0; // radians
-    
-    char samples_filename[] = "sampled_sine_wave.txt";
-    FILE *samples_fp = fopen(samples_filename, "w");
-    if(samples_fp == NULL) {
+    FILE *fp = fopen("signal.txt", "w");
+    if(fp == NULL) {
         perror("fopen");
         return -1;
     }
-
-    fprintf(samples_fp, "sine_freq[Hz]: %.6f\n", sine_freq);
-    fprintf(samples_fp, "initial_phase[rad]: %.6f\n", initial_phase);
-    fprintf(samples_fp, "sampling_freq[Hz]: %.6f\n", SAMPLING_FREQ);
-    fprintf(samples_fp, "sampling_time[s]: %.6f\n", SAMPLING_TIME);
-    fprintf(samples_fp, "samples_len: %ld\n", SAMPLES_LEN);
-
-    for(size_t i=0; i<SAMPLES_LEN; i++) {
-        double freq_ratio = sine_freq / SAMPLING_FREQ;
-        double t = i * SAMPLING_TIME/SAMPLES_LEN;
-        samples[i] = sin(2*M_PI * i * freq_ratio + initial_phase);
-        fprintf(samples_fp, "%.6f %.6f\n", t, samples[i]);
+    fprintf(fp, "sine_freqs[Hz]: ");
+    for(size_t freq_index=0; freq_index<num_freqs; freq_index++) {
+        fprintf(fp, "%.6f ", sine_freqs[freq_index]);
     }
-    fclose(samples_fp);
+    fprintf(fp, "\ninitial_phases[rad]: ");
+    for(size_t freq_index=0; freq_index<num_freqs; freq_index++) {
+        fprintf(fp, "%.6f ", initial_phases[freq_index]);
+    }
+    fprintf(fp, "\n");
+    for(size_t signal_point=0; signal_point<signal_len; signal_point++) {
+        double t = signal_point * sampling_time / signal_len;
+        fprintf(fp, "%.6f %.6f\n", t, signal[signal_point]);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
+
+int generate_sampled_signal(
+    double* samples, double sampling_freq, double samples_len,
+    double sine_freqs[], double initial_phases[], size_t num_freqs
+) {
+    for(size_t i=0; i<samples_len; i++) {
+        double t = i / sampling_freq;
+        for(size_t freq_index=0; freq_index<num_freqs; freq_index++) {
+            samples[i] += sin(2*M_PI * sine_freqs[freq_index] * t + initial_phases[freq_index]);
+        }
+    }
+
+    FILE *fp = fopen("samples.txt", "w");
+    if(fp == NULL) {
+        perror("fopen");
+        return -1;
+    }
+    
+    for(size_t i=0; i<samples_len; i++) {
+        double t = i / sampling_freq;
+        fprintf(fp, "%.6f %.6f\n", t, samples[i]);
+    }
+
+    return 0;
+    // DOKOŃCZ ZAPIS DO PLIKU I ZOBACZ CZY DZIALA BO NIE MA SENSU MIEC SYGNALU JAK NIE JEST ANALOGOWY-
+}
+
+
+int sample_signal(
+    double *signal, size_t signal_len,
+    double *samples, size_t samples_len,
+    double sampling_time
+) {
+    size_t signal_sample_len_ratio = signal_len / samples_len; 
+    for(size_t i=0; i<samples_len; i++) {
+        size_t sampling_point = i * (signal_sample_len_ratio);
+        samples[i] = signal[sampling_point];
+    }
+
+    FILE *fp = fopen("samples.txt", "w");
+    if(fp == NULL) {
+        perror("fopen");
+        return -1;
+    }
+    fprintf(fp, "sampling_time[s]: %f\n", sampling_time);
+    for(size_t i=0; i<samples_len; i++) {
+        fprintf(fp, "%.6f\n", samples[i]);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
+
+int main() {
+
+    double SAMPLING_FREQ = 1024.0;   // [Hz]
+    double SAMPLING_TIME = 1.0;    // [s]
+    size_t SAMPLES_LEN = SAMPLING_FREQ * SAMPLING_TIME;
+    size_t SIGNAL_LEN  = 10 * SAMPLES_LEN;
+    double NYQUIST_FREQ = SAMPLES_LEN / 2.0; // [Hz] max frequency that can be reliably extracted from the signal
+    
+    // Specifying frequency components of a signal ================================================
+    double sine_freqs[]     = {2.3, 5.7, 11.2, 27.77}; // [Hz]  
+    double initial_phases[] = {M_PI/7.0, M_PI/2.3,  0.0,  11.7}; // [rad]
+    size_t num_freqs        = sizeof(sine_freqs)/sizeof(sine_freqs[0]);
+
+    // Allocating memory for signal ===============================================================
+    double *signal = (double*)calloc(SIGNAL_LEN, sizeof(double));
+    // Allocating memory for samples vector and zero initializing it ==============================
+    double *samples = (double*)calloc(SAMPLES_LEN, sizeof(double));
+
+    // Generating desired signal ==================================================================
+    generate_signal(signal, SIGNAL_LEN, SAMPLING_TIME, sine_freqs, initial_phases, num_freqs);
+    // Sampling the signal
+    // sample_signal(signal, SIGNAL_LEN, samples, SAMPLES_LEN, SAMPLING_TIME);
+    generate_sampled_signal(samples, SAMPLING_FREQ, SAMPLES_LEN, sine_freqs, initial_phases, num_freqs);
 
     // Allocating memory for the Fourier Transform Matrix =========================================
     double complex **dft_matrix = (double complex**)malloc(SAMPLES_LEN * sizeof(double complex*));
--- plot_freq_spectrum.py
+++ plot_freq_spectrum.py
@@ -2,42 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-with open("sampled_sine_wave.txt", "r") as f:
-    line = f.readline()         # "sine_freq[Hz]: <sine_freq>\n"
-    sine_freq       = float(line.split(":")[1])
-    
-    line = f.readline()
-    initial_phase   = float(line.split(":")[1])
-    
-    line = f.readline()         # "sampling_freq[Hz]: <sampling_freq>\n"
-    sampling_freq   = float(line.split(":")[1])
-    
-    line = f.readline()         # "sampling_time[s]: <sampling_time>\n"
-    sampling_time   = float(line.split(":")[1])
-    
-    line = f.readline()         # "samples_len: <samples_len>\n"
-    samples_len     = int(line.split(":")[1])
+with open("signal.txt", "r") as f:
+    f.readline()
+    f.readline()
 
-    samples_data    = np.loadtxt(f)
-    sampled_time, samples = samples_data[:,0], samples_data[:,1]
+    signal_data = np.loadtxt(f)
+    signal_time, signal = signal_data[:,0], signal_data[:,1]
 
-freq_ratio = sine_freq / sampling_freq
+with open("samples.txt", "r") as f:
+    # line = f.readline() # "sine_freqs[Hz]: <sine_freq0> <sine_freq1> [...]\n"
+    # sine_freqs      = np.array((line.split(":")[1]).split(" ")[1:], dtype=float)
+    
+    # line = f.readline() # "sine_phases[rad]: <init_phase0> <init_phase1> [...]\n" 
+    # initial_phase   = np.array((line.split(":")[1]).split(" ")[1:], dtype=float)
+    
+    # line = f.readline() # "sampling_freq[Hz]: <sampling_freq>\n"
+    # sampling_freq   = float(line.split(":")[1])
+    
+    # line = f.readline() # "sampling_time[s]: <sampling_time>\n"
+    # sampling_time   = float(line.split(":")[1])
+    
+    # line = f.readline() # "samples_len: <samples_len>\n"
+    # samples_len     = int(line.split(":")[1])
 
-print(f"sine_freq[Hz]: {sine_freq}")   
-print(f"initial_phase[rad]: {initial_phase}")
-print(f"sampling_freq[Hz]: {sampling_freq}")
-print(f"sampling_time[s]: {sampling_time}")
-print(f"samples_len: {samples_len}")
+    samples_data = np.loadtxt(f)
+    samples_time, samples = samples_data[:,0], samples_data[:,1]
+
+# print(f"sampling_freq[Hz]: {sampling_freq}")
+# print(f"sampling_time[s]: {sampling_time}")
+# print(f"samples_len: {samples_len}")
 
 with open("freq_magnitudes_phases.txt") as file:
     freq_magnitudes_phases = np.loadtxt(file)
 freq, freq_mag, freq_phase = freq_magnitudes_phases[:,0], freq_magnitudes_phases[:,1], freq_magnitudes_phases[:,2]
 
-time_original_signal = np.linspace(0.0, sampling_time, 10*samples_len)
-
+# Plotting results ==================================================================
 fig, axes = plt.subplots(2, 1)
-axes[0].plot(time_original_signal, np.sin(2*math.pi * time_original_signal * sine_freq + initial_phase), c='b', label="signal", zorder=1)
-axes[0].scatter(sampled_time, samples, s=2, c='r', label="samples", zorder=2)
+axes[0].plot(signal_time, signal, c='b', label="signal", zorder=1)
+axes[0].scatter(samples_time, samples, s=1.5, c='r', label="samples", zorder=2)
 axes[0].set_xlabel("time [s]")
 axes[0].set_ylabel("amplitude [V]") 
 axes[0].set_title("Original signal (blue) and sampled points (red)")
